b8e5daf509184ef6735071896ccd830c56891437
HinGutBoy <trunghiennguyen71@gmail.com>
2026-03-16 20:53:27 +0700

    Update DB info

--- back-end/src/main/java/com/vti/vti_champion/Main.java
+++ back-end/src/main/java/com/vti/vti_champion/Main.java
@@ -1,11 +1,17 @@
 package com.vti.vti_champion;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.out.println(System.getProperty("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         SpringApplication.run(Main.class, args);
     }
 }
